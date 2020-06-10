cc668d0ea6f76a34f9c6a05a32d1d2f5339797ce
Char-cp <www.98985@qq.com>
2020-06-10 15:32:45 +0800

    emmm

---
+++ src/main/test/modo/Doge.java
@@ -0,0 +1,77 @@
+package modo;
+
+public class Doge {
+
+    protected String amount;
+    protected String open;
+    protected String close;
+    protected String high;
+    protected String id;
+    protected String count;
+    protected String low;
+    protected String vol;
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public void setClose(String close) {
+        this.close = close;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public void setVol(String vol) {
+        this.vol = vol;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public String getClose() {
+        return close;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public String getVol() {
+        return vol;
+    }
+}
